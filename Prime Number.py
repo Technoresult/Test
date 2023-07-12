9472476b2b0dc637f78a1ace506374f1a740cf05
Premkumar Murugesan <premkumar.murugesan@soroco.com>
2023-07-12 18:05:11 +0530

    First commit

---
+++ Prime Number.py
@@ -0,0 +1,11 @@
+print("Printing Dates with the Seperator Operator")
+X=input("What is the Date:")
+Y=input("What is the Month:")
+Z=int(input("What is the Year:"))
+print(X, Y, Z, sep=("-"))
+print("Calculating your age")
+Age=2023-Z
+print("Your age is", +Age)
+
+
+
