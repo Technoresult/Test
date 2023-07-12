59619b6ae0668f3e88b71fd5b19bd571d303d8c0
Premkumar Murugesan <premkumar.murugesan@soroco.com>
2023-07-12 18:12:50 +0530

    New file added

---
+++ List.py
@@ -0,0 +1,14 @@
+print("*****List Practice*******")
+Cars=["Audi","Benz","Maruthi","Toyota"]
+print(Cars)
+print(Cars[0])
+print(Cars[1])
+print(Cars[2])
+print(Cars[3])
+print(Cars[-2])
+Cars[2]="Grant"
+print(Cars)
+Cars.append("Maruthi")
+print(Cars)
+Cars.remove("Toyota")
+print(Cars)
